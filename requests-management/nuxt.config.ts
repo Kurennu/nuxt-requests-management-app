aca88b96c684113ec20cf43cb573fee0b9260a96
kuren <aleksandrapettrova@gmail.com>
2026-02-09 19:30:25 +1000

    dosc: add readme

--- requests-management/nuxt.config.ts
+++ requests-management/nuxt.config.ts
@@ -1,9 +1,19 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
+    compatibilityDate: '2025-07-15',
+    devtools: { enabled: true },
     typescript: {
         strict: true
+    },
+    app: {
+        head: {
+            htmlAttrs: {
+                lang: 'ru'
+            },
+            title: 'Управление заявками',
+            link: [
+                { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
+            ]
+        }
     },
     ssr: false,
     css: ['~/assets/styles/main.scss'],
